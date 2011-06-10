d02ec8054c99f1f44faae0274dbbccba07fe90e3
Oliver Schulz <oliver.schulz@tu-dortmund.de>
2011-06-10 19:40:21 +0200

    Extended VectorOps::draw

--- src/VectorOps.cxx
+++ src/VectorOps.cxx
@@ -56,4 +56,23 @@ TGraph* VectorOps::graph(const std::vector<float> &y) { return graphT(y); }
 TGraph* VectorOps::graph(const std::vector<double> &y) { return graphT(y); }
 
 
+void VectorOps::draw(const std::vector<int16_t> &x, const std::vector<int16_t> &y, Option_t* chopt, Color_t lineColor)
+	{ TGraph *g = graph(x, y); g->SetLineColor(lineColor); g->Draw(chopt); }
+void VectorOps::draw(const std::vector<int32_t> &x, const std::vector<int32_t> &y, Option_t* chopt, Color_t lineColor)
+	{ TGraph *g = graph(x, y); g->SetLineColor(lineColor); g->Draw(chopt); }
+void VectorOps::draw(const std::vector<float> &x, const std::vector<float> &y, Option_t* chopt, Color_t lineColor)
+	{ TGraph *g = graph(x, y); g->SetLineColor(lineColor); g->Draw(chopt); }
+void VectorOps::draw(const std::vector<double> &x, const std::vector<double> &y, Option_t* chopt, Color_t lineColor)
+	{ TGraph *g = graph(x, y); g->SetLineColor(lineColor); g->Draw(chopt); }
+
+void VectorOps::draw(const std::vector<int16_t> &y, Option_t* chopt, Color_t lineColor)
+	{ TGraph *g = graph(y); g->SetLineColor(lineColor); g->Draw(chopt); }
+void VectorOps::draw(const std::vector<int32_t> &y, Option_t* chopt, Color_t lineColor)
+	{ TGraph *g = graph(y); g->SetLineColor(lineColor); g->Draw(chopt); }
+void VectorOps::draw(const std::vector<float> &y, Option_t* chopt, Color_t lineColor)
+	{ TGraph *g = graph(y); g->SetLineColor(lineColor); g->Draw(chopt); }
+void VectorOps::draw(const std::vector<double> &y, Option_t* chopt, Color_t lineColor)
+	{ TGraph *g = graph(y); g->SetLineColor(lineColor); g->Draw(chopt); }
+
+
 } // namespace sigpx
